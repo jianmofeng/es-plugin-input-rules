ca4ebe5c3a45ccbebaf3556770ee4bb0bcee1dc5
见风 <s543355165@163.com>
2021-11-28 22:45:12 +0800

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,38 +1,5 @@
 "use strict";
 
-// module.exports = {
-//   root: true,
-//   extends: [
-//     // "eslint:recommended",
-//     // "plugin:eslint-plugin/recommended",
-//     // "plugin:node/recommended",
-//     'plugin:vue/vue3-recommended',
-//   ],
-//   parser: "vue-eslint-parser",
-//   env: {
-//     node: true,
-//   },
-//   overrides: [
-//     {
-//       files: ["tests/**/*.js"],
-//       env: { mocha: true },
-//     },
-//   ],
-//   parserOptions: {
-//     sourceType: 'module',
-//     ecmaVersion: 2018,
-//     ecmaFeatures: {
-//         globalReturn: false,
-//         impliedStrict: false,
-//         jsx: false
-//     },
-//     vueFeatures: {
-//         filter: true,
-//         interpolationAsNonHTML: false,
-//         styleCSSVariableInjection: true,
-//     },
-//   },
-// };
 module.exports = {
   root: true,
   parserOptions: {
