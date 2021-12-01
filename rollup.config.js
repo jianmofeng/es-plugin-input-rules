e17844b6160c2e8c4a7e140e811cd15f86ac003c
chenhao.jia <chenhao.jia@xiaobao100.com>
2021-12-01 15:18:05 +0800

    chore: 增加rollup配置
    undefined

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,52 @@
+import resolve from 'rollup-plugin-node-resolve';
+import babel from 'rollup-plugin-babel';
+import commonjs from 'rollup-plugin-commonjs'
+import json from 'rollup-plugin-json';
+import typescript from 'rollup-plugin-typescript'
+import { terser } from "rollup-plugin-terser";
+
 export default {
-  input: 'lib/index.js',
-  output: {
-    file: 'dist/bundle.js',
-    format: 'cjs'
+    input: 'lib/index.js',
+    output: {
+        file: 'dist/index.js',
+        format: 'cjs',
     },
-  plugins: [ json() ]
+    // sourceMap: 'inline',
+    plugins: [
+        resolve(),
+        commonjs({
+            include: /node_modules/
+        }),
+        
+        typescript(),
+        // babel({
+        //     exclude: 'node_modules/**',
+        //     runtimeHelpers: true,
+        //     externalHelpers: true
+        // }),
+        babel({ runtimeHelpers: true, externalHelpers: true, exclude: 'node_modules/**' }),
+        terser(),
+        json({
+            // 默认情况下将解析所有JSON文件,
+            // 但您可以专门包含/排除文件
+            // include: 'node_modules/**',
+            exclude: [ 'node_modules' ],
+
+            // 对于 tree-shaking, 属性将声明为npm install --save-dev babel-helpers
+            // 变量, 使用 `var` 或者 `const`
+            preferConst: true, // 默认是 false
+
+            // 为生成的默认导出指定缩进 —
+            // 默认为 't'
+            indent: '  ',
+
+            // 忽略缩进并生成最小的代码
+            compact: true, // 默认是 false
+
+            // 为JSON对象的每个属性生成一个命名导出
+            namedExports: true // 默认是 true
+        })
+    ],
+    //排除第三方依赖
+    // external: ["moment"]
 };
